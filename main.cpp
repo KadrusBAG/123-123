38f8309f067235f5c35ca55183827223a2e66ffc
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2017-12-01 10:56:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,24 +14,46 @@ void add(node * & head){
     int value;
     stream >> value;
     node * time = new node;
-    time -> val = value;
-    time -> next = nullptr;
+    time->val = value;
+    time->next = nullptr;
     if(head == nullptr){
         head = time;
     }
     else{
         node * last = head;
-        while(last -> next){
-            last = last -> next;
+        while(last->next){
+            last = last->next;
         }
-        last -> next = time;
+        last->next = time;
     }
 }
 
 void sub(node * & head){
     node * time = new node;
-    time = head;
+    time = head->next;
     delete head;
+    head = time;
+}
+
+void reverse(node * & head){
+    int k = 1;
+    node * left = new node;
+    left = head;
+    node * right = head;
+    while(right->next){
+        right = right->next;
+        k++;
+    }
+    for(int i = 0; i < k/2; ++i){
+        swap(left, right);
+        left = left->next;
+        right = right->prev;
+    }
+    head = left;
+}
+
+void output(node * head){
+    
 
 int main{
     node * head = nullptr;
@@ -41,4 +63,16 @@ int main{
             add(head);
         }
         if(op == '-'){
-            
+            sub(head);
+        }
+        if(op == '/'){
+            reverse(head);
+        }
+        if(op == '='){
+            output(head);
+        }
+        if(op == 'q'){
+            break;
+        }
+    }
+}  
